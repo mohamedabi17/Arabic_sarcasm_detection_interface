2957a431d09d7d728624a5858b541f1f0a52796e
mohamedabi17 <mohamedabi1485@gmail.com>
2024-01-26 20:15:40 +0100

    'fixbugs

--- flaskapi/models/sarcasm_model_creator.py
+++ flaskapi/models/sarcasm_model_creator.py
@@ -2,13 +2,13 @@
 import joblib
 def load_model():
     # Load and return the trained RandomForestClassifier model
-    model = joblib.load('models/sarcasm_model_v4.joblib') 
+    model = joblib.load('models/best_sarcasm_model.joblib') 
     return model
 def load_vectorizer():
     # Load and return the trained RandomForestClassifier model
-    vectorizer = joblib.load('models/vectorizer.joblib') 
+    vectorizer = joblib.load('models/vectorizer_v5.joblib') 
     return vectorizer
 def load_encoder():
     # Load and return the trained RandomForestClassifier model
-    encoder = joblib.load('models/encoder.joblib') 
+    encoder = joblib.load('models/encoder_test_v5.joblib') 
     return encoder
